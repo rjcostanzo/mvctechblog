c8b479a1880bd4458c36b5901a2d95f71d99b5a7
rjcostanzo <rjcostanzo@gmail.com>
2021-11-15 17:05:35 -0600

    config/connection.js setup, in addition to installing dependencies and writing /models

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS techblogdb;
+
+CREATE DATABASE techblogdb;
---
+++ models/Comment.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+// create Comment model
+class Comment extends Model {}
+
+Comment.init({
+// sequelize figures out the id as a primary key for us
+    id: {
+// set our value types: INTEGER, STRING, FLOAT, etc.
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+// this makes the column auto increment
+        autoIncrement: true
+    },
+    comment_text: {
+        type: DataTypes.STRING,
+        validate: {
+
+            len: [3]
+        }
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'post',
+            key: 'id'
+        }
+    }
+}, {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+});
+module.exports = Comment;
