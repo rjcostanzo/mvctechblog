1953b5aaf17a3e59b5670437594d28855b033d4c
rjcostanzo <rjcostanzo@gmail.com>
2021-11-21 14:25:34 -0600

    connection.js re-fix for port issue on heroku end

--- config/connection.js
+++ config/connection.js
@@ -8,9 +8,8 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-       host: 'localhost',
-       dialect: 'mysql',
-       port: 3306
+       host: '127.0.0.1',
+       dialect: 'mysql'
     });
 };
 module.exports = sequelize;
