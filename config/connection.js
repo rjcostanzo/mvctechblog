2db901dab25a2a936902074c4cee5c4ce9e15a01
rjcostanzo <rjcostanzo@gmail.com>
2021-11-21 14:03:52 -0600

    sequelize port issue fix

--- config/connection.js
+++ config/connection.js
@@ -9,8 +9,7 @@ if (process.env.JAWSDB_URL) {
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
        host: 'localhost',
-       dialect: 'mysql',
-       port: 3306
+       dialect: 'mysql'
     });
 };
 module.exports = sequelize;
